167cabe7fc56da74b313e4d7dee9dbb6ddfcb5bc
TheColorRed <lucasgreer2@gmail.com>
2022-02-08 11:48:10 -0700

    Added warns

--- chocolatebot.py
+++ chocolatebot.py
@@ -2,6 +2,13 @@ import discord
 import discord.ext.commands
 from better_profanity import profanity
 import os
+import json
+
+warns = {}
+
+with open("./warns.json", "r") as file:
+    if file:
+        warns = json.load(file)
 
 bot = discord.ext.commands.Bot("c!")
 
@@ -10,5 +17,17 @@ async def on_message(message):
     if profanity.contains_profanity(message.content):
         #await message.edit(content=profanity.censor(message.content))
         await message.channel.send("Don't swear, " + message.author.mention + "! You have been warned.")
-        
+        warn(message.author)
+
+@bot.command()        
+async def warn(uid: discord.Member):
+    global warns
+    if warns[uid]:
+        warns[uid] += 1
+        if warns[uid] == 7:
+            await uid.ban()
+            await uid.send("You have been banned for gaining too many warns.")
+        return
+    warns[uid] = 1
+
 bot.run(os.getenv("DISCORD_TOKEN"))
