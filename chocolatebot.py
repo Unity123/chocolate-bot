0a563335f73e36b1f2b9393d629b330176e58c89
Red <lucasgreer2@gmail.com>
2022-02-08 17:31:57 -0700

    e

--- chocolatebot.py
+++ chocolatebot.py
@@ -15,7 +15,7 @@ bot = discord.ext.commands.Bot("c!")
 @bot.listen()
 async def on_message(message):
     if profanity.contains_profanity(message.content):
-        #await message.edit(content=profanity.censor(message.content))
+        await message.edit(content=profanity.censor(message.content))
         await message.channel.send("Don't swear, " + message.author.mention + "! You have been warned.")
         await _warn(message.author)
 
